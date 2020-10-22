96ad90220d57508930bfbbb8d856eb21c2ff75d0
unknown <mstuck@asu.edu>
2020-10-22 10:55:52 -0700

    Updates

---
+++ precache-manifest.85fed32813752236eec7320dc0689551.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa21767b9333e803a13479c2448bb2dd",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "a3a1ae91fb36cf75e946",
+    "url": "/Covid-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "074cc7e69cacc2bf9094",
+    "url": "/Covid-Tracker/static/css/main.722b9050.chunk.css"
+  },
+  {
+    "revision": "a3a1ae91fb36cf75e946",
+    "url": "/Covid-Tracker/static/js/2.895a631e.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-Tracker/static/js/2.895a631e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074cc7e69cacc2bf9094",
+    "url": "/Covid-Tracker/static/js/main.037e84f2.chunk.js"
+  },
+  {
+    "revision": "e098ef0056ce6f3c05cb",
+    "url": "/Covid-Tracker/static/js/runtime-main.e7525490.js"
+  }
+]);
